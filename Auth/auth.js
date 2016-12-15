0e7f22d68a7ac56ebe38fa7802b6e148c9188eb2
Vamci <Kalidasu.Surada@gmail.com>
2016-12-15 19:31:28 +0530

    modified files

--- Auth/auth.js
+++ Auth/auth.js
@@ -70,7 +70,7 @@ Auth.prototype.addUserRoles = function(req,res){
 
 Auth.prototype.validateLogin = function(req,res){
 
-	var query = "select top 1* from sfusers where UserName = '" + req.body.UserName + "' and Password = '" + req.body.Password +"'";
+	var query = "select * from sfusers where UserName = '" + req.body.UserName + "' and Password = '" + req.body.Password +"'";
 	connection.query(query,function(err,result){
 		if(err)
 		{
